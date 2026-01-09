fe83c43f611ff6d3ad7220cc0056a85b27cb5e7c
Lauritz Christian Holme <lauritz@holme.app>
2026-01-09 14:25:29 +0100

    Initial cookiecutter template

---
+++ src/lucky_ai/train.py
@@ -0,0 +1,10 @@
+from lucky_ai.model import Model
+from lucky_ai.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
